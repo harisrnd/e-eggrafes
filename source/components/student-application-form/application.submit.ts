8053e3844c80577fdb4cbf9ea6616d1a633d0622
Aspasia Katsi <aspakatsi@yahoo.gr>
2018-03-01 08:19:58 +0200

    chages

--- source/components/student-application-form/application.submit.ts
+++ source/components/student-application-form/application.submit.ts
@@ -410,8 +410,8 @@ import { StudentCourseChosen, StudentEpalChosen, StudentSectorChosen } from "../
         }
 
                        
-     //       this.ServiceStudentCertifSub = this._hds.getServiceStudentCertification(8)
-              this.ServiceStudentCertifSub = this._hds.getServiceStudentPromotion('24','null','null','null','null','04-01-1997','0540961','777')     
+        //      this.ServiceStudentCertifSub = this._hds.getServiceStudentCertification(8)
+              this.ServiceStudentCertifSub = this._hds.getServiceStudentPromotion('25','null','null','null','null','11-02-2001','0540860','2044')     
                 .subscribe(data => {
                     this.ServiceStudentCertif$.next(data);
                     
