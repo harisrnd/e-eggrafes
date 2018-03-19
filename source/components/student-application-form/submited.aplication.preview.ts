7c74bcf4bf9ecd0d5d768de6f4b72412e337b3cc
Nikos Katsaounos <nkatsaounos@sch.gr>
2018-03-19 14:26:57 +0200

    minor changes

--- source/components/student-application-form/submited.aplication.preview.ts
+++ source/components/student-application-form/submited.aplication.preview.ts
@@ -108,7 +108,7 @@ import { IAppState } from "../../store/store";
 
                 <div class="col-md-5" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveGelUser(UserData$.id)">{{UserData$.studentsurname}}</div>
                 <div class="col-md-4" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveGelUser(UserData$.id)">{{UserData$.name}}</div>
-                <div class="col-md-2" *ngIf = "(GelSubmittedDetails$ | async).length >0" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveGelUser(UserData$.id)">ΓΕΛ</div>
+                <div class="col-md-2" *ngIf = "(GelSubmittedDetails$ | async).size" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveGelUser(UserData$.id)">ΓΕΛ</div>
                 <div *ngIf="UserData$.candelete === 1" class="col-md-1 text-right" style="font-size: 1em; font-weight: bold;"><i class="fa fa-trash isclickable" (click)="deleteGelApplication(UserData$.id)"></i></div>
                 <div *ngIf="UserData$.candelete === 0" class="col-md-1" style="font-size: 1em; font-weight: bold;">&nbsp;</div>
 
@@ -265,15 +265,15 @@ import { IAppState } from "../../store/store";
             <div class="row list-group-item isclickable"  style="margin: 0px 2px 0px 2px;" [class.oddout]="isOdd" [class.evenout]="isEven" [class.selectedappout]="applicationEpalIdActive === UserData$.id"
             *ngFor="let UserData$  of SubmitedApplic$ | async; let i=index; let isOdd=odd; let isEven=even" >
 
-                <div *ngIf = "(EpalSubmittedDetails$ | async).size">
+                <!--<div *ngIf = "(EpalSubmittedDetails$ | async).size">-->
 
                 <div class="col-md-5" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveEpalUser(UserData$.id)">{{UserData$.studentsurname}}</div>
                 <div class="col-md-4" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveEpalUser(UserData$.id)">{{UserData$.name}}</div>
-                <div class="col-md-2" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveEpalUser(UserData$.id)">ΕΠΑΛ</div>
+                <div class="col-md-2" *ngIf = "(EpalSubmittedDetails$ | async).size" style="font-size: 0.8em; font-weight: bold;" (click)="setActiveEpalUser(UserData$.id)">ΕΠΑΛ</div>
                 <div *ngIf="UserData$.candelete === 1" class="col-md-1 text-right" style="font-size: 1em; font-weight: bold;"><i class="fa fa-trash isclickable" (click)="deleteApplication(UserData$.id)"></i></div>
                 <div *ngIf="UserData$.candelete === 0" class="col-md-1" style="font-size: 1em; font-weight: bold;">&nbsp;</div>
 
-                </div>
+
 
                 <div style="width: 100%">
                 <div *ngFor="let StudentDetails$  of EpalSubmittedDetails$ | async" [hidden]="UserData$.id !== applicationEpalIdActive" style="margin: 10px 10px 10px 10px;">
