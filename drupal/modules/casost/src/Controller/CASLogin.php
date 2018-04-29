f75c9ebe46594420ed6aecc81479b3f0025d69c3
Aspasia Katsi <aspakatsi@yahoo.gr>
2018-04-29 12:43:54 +0300

    changes

--- drupal/modules/casost/src/Controller/CASLogin.php
+++ drupal/modules/casost/src/Controller/CASLogin.php
@@ -145,14 +145,13 @@ class CASLogin extends ControllerBase
     //        $gsnunitcodedn = $filterAttribute('edupersonorgunitdn:gsnunitcode:extended');
     //        $gsnunitcode = substr($gsnunitcodedn, strpos($gsnunitcodedn, ";") + 1);
             $gsnunitcode = $filterAttribute('edupersonorgunitdn:gsnunitcode');
-/* check if myschool account */
+
             if (!$umdobject || $umdobject !== "ISaccount") {
                 return $this->redirectForbidden($configRowName, '5002');
             }
             if (!$gsnunitcode || $gsnunitcode !== $CASUser) {
                 return $this->redirectForbidden($configRowName, '5003');
             }
-/* end of checking myschool account */
 
             $userAssigned = $this->assignRoleToUser($gsnunitcode);
 
